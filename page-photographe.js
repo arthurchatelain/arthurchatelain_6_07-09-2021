d161cc4cc2741d55e930aea9a850b543dceb6d8b
Arthur <arthurchatelain72@gmail.com>
2021-09-27 19:03:33 +0200

    ajout header photographe et modal de contact

--- page-photographe.js
+++ page-photographe.js
@@ -38,14 +38,64 @@ function HeaderPhotographe(number){
         tag.textContent = "#" + data.photographers[number].tags[i].toLowerCase();
         tag.className = "tags";
         filtres.appendChild(tag);   
-    } 
-    articlephotographe.appendChild(prenom);
-    articlephotographe.appendChild(lieu);
-    articlephotographe.appendChild(sentence);
-    articlephotographe.appendChild(filtres);
+    }
+    let divarticle = document.createElement('div');
+    divarticle.className = 'divarticle';
+    let boutoncontact = document.createElement('button');
+    boutoncontact.className = 'openmodal openmodalcontact';
+    boutoncontact.textContent = 'Contactez-Moi';
+    divarticle.appendChild(prenom);
+    divarticle.appendChild(lieu);
+    divarticle.appendChild(sentence);
+    divarticle.appendChild(filtres);
+    articlephotographe.appendChild(divarticle);
+    articlephotographe.appendChild(boutoncontact);
     photographe.appendChild(articlephotographe);
     photographe.appendChild(divimg);
     document.getElementById('main').appendChild(photographe);
+    let modalmenu = document.createElement('div');
+    modalmenu.className = 'modalmenu';
+    let modaltitre = document.createElement('h1');
+    modaltitre.className = 'modaltitre';
+    modaltitre.textContent = 'Contactez-Moi';
+    let modalclose = document.createElement('span');
+    modalclose.className = 'modalclose';
+    modalmenu.appendChild(modaltitre);
+    modalmenu.appendChild(modalclose);
+    let modalnom = document.createElement('p');
+    modalnom.className = 'modalnom';
+    modalnom.textContent = data.photographers[number].name;
+    document.getElementById('contenermodaltexte').appendChild(modalmenu);
+    document.getElementById('contenermodaltexte').appendChild(modalnom);
+
 }
 
 HeaderPhotographe(numeroduphotographe);
+
+// fonctionalités modals 
+
+let modalbg = document.getElementById('modalbg');
+let openmodal = document.querySelectorAll(".openmodal");
+let fermerModal = document.querySelectorAll(".modalclose");
+
+function launchModal() {
+    modalbg.style.display = "flex";
+}
+function CloseModal() {
+    modalbg.style.display = "none";
+}
+function submit () {
+    console.log("Prénom : " + document.getElementById('first').value);
+    console.log("Nom : " + document.getElementById('last').value);
+    console.log("Email : " + document.getElementById('email').value);
+    console.log("Message : " + document.getElementById('message').value);
+    modalbg.style.display = "none";
+}
+openmodal.forEach((btn) => btn.addEventListener("click", launchModal));
+fermerModal.forEach((btn) => btn.addEventListener("click", CloseModal));
+
+document.getElementById('btn-submit').addEventListener('click', function (e) {
+    e.preventDefault();
+    submit();   
+  })
+
